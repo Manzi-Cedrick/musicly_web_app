80cc9e099860f739ad0013ebdccf43e3db44307c
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-11 21:20:07 +0200

    [add] color additional configuration --config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,tsx,ts}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        'main':'#FB2576',
+      }
+    },
+  },  
   plugins: [],
 }
