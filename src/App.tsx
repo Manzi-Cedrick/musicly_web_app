f80d087494499420df4be659c118d784682744f1
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-17 20:45:13 +0200

    [update] pages config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Album from './pages/albums'
+import CreateSong from './pages/createSong'
 import Dashboard from './pages/dashboard'
+import Genre from './pages/genre'
+import Songs from './pages/songs'
 
 export const App = () => {
     return (
        <>
-       <Dashboard/>
+       <Routes>
+            <Route element={<Dashboard/>} path='/'/>
+            <Route element={<Album/>} path='/albums'/>
+            <Route element={<Songs/>} path='/songs'/>
+            <Route element={<Genre/>} path='/music/genre'/>
+            <Route element={<CreateSong/>} path='/songs/new'/>
+       </Routes>
        </>
     )
 }
