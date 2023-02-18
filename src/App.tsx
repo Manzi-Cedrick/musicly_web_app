16ba166509bb79c9b2350783b62360e28b17fa69
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-18 19:58:13 +0200

    [update] page addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Album from './pages/albums'
+import CreateAlbum from './pages/createAlbum'
 import CreateSong from './pages/createSong'
 import Dashboard from './pages/dashboard'
 import Genre from './pages/genre'
+import GenreDetails from './pages/genreDetails'
+import Login from './pages/login'
+import SignUp from './pages/signup'
 import Songs from './pages/songs'
+import UpdateAlbum from './pages/updateAlbum'
+import UpdateSong from './pages/updateSong'
 
 export const App = () => {
     return (
@@ -15,6 +21,12 @@ export const App = () => {
             <Route element={<Songs/>} path='/songs'/>
             <Route element={<Genre/>} path='/music/genre'/>
             <Route element={<CreateSong/>} path='/songs/new'/>
+            <Route element={<UpdateSong />} path='/songs/:id/update' />
+            <Route element={<CreateAlbum />} path='/album/new'/>
+            <Route element={<UpdateAlbum />} path='/albums/:id/update' />
+            <Route element={<GenreDetails />} path='/genre/:id/details' />
+            <Route element={<Login />} path='/login'/>
+            <Route element={<SignUp/>} path='/signup' />
        </Routes>
        </>
     )
