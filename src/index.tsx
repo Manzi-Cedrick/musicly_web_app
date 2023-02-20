1fdc00f2b386d14defa289ac8cc1ba25f9714574
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-20 23:09:26 +0200

    [update] toastify addition

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {App} from './App';
+import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <BrowserRouter>
-      <App />
+    <ToastContainer style={{ fontSize: '0.8em' }} />
+    <App />
   </BrowserRouter>
 
 );
