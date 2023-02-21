cd381c59d63dfe58cad3559a4d359a3b56510861
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-21 23:04:58 +0200

    [update] songs unit integration --testing

--- src/components/songTable.tsx
+++ src/components/songTable.tsx
@@ -2,10 +2,13 @@ import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { albumDataArr, albumInt } from '../utils/album.data';
 import DeleteData from './modals/deleteData'
+import DeleteSongData from './modals/deleteSongData';
 
-const SongTable = () => {
-    const [albumData, setalbumData] = useState(albumDataArr);
-    const [modal, setModal] = useState(false);
+const SongTable = ({data}:{data:any}) => {
+    const [modal, setModal] = useState({
+        id:'',
+        showModal:false
+    });
     const navigate = useNavigate();
     const handleUpdate = (id: number) => {
         navigate(`/songs/${id}/update`);
@@ -17,28 +20,27 @@ const SongTable = () => {
                 <th>Name of Song</th>
                 <th>Artist </th>
                 <th>Time</th>
-                <th>Like</th>
+                <th>Genre</th>
                 <th>Actions</th>
             </tr>
-            {albumData.map((album: albumInt, i: number) => (
-                <tr className="border-b-2 hover:bg-slate-900 text-center hover:cursor-pointer border-gray-500 text-[12px] font-semibold">
+            {data.map((song: any, i: number) => (
+                <tr key={song.song_id} className="border-b-2 hover:bg-slate-900 text-center hover:cursor-pointer border-gray-500 text-[12px] font-semibold">
                     <td className='text-center'>
                         # {i}
                     </td>
                     <td className='flex gap-8 py-5 justify-center place-items-center text-center'>
-                        <img className='w-8 h-8 object-cover rounded-full' src={`${album.cover_image}`} alt="" />
-                        <span>{album.title}</span>
+                        <span>{song.title}</span>
                     </td>
-                    <td className='text-center'>{album.artist}</td>
-                    <td className='text-center'>{i}</td>
-                    <td className='text-center'>{i}</td>
+                    <td className='text-center'>{song.artist}</td>
+                    <td className='text-center'>{song.length}</td>
+                    <td className='text-center'>{song.genre}</td>
                     <td className='text-center justify-center'>
                         <button onClick={() => handleUpdate(i)} className='bg-main hover:bg-white hover:text-main duration-500 border-2 border-white opacity-95 mx-4 text-black py-1 px-6 text-[12px] rounded-md'>Update</button>
-                        <button onClick={() => setModal(true)} className='bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
+                        <button onClick={() => setModal({ ...modal, id: song.song_id, showModal: true })} className='bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
                     </td>
                 </tr>
             ))}
-            <DeleteData id={"2312323"} showModal={modal} onClose={() => setModal(false)} />
+            <DeleteSongData id={modal.id} showModal={modal.showModal} onClose={() => setModal({ ...modal, showModal: false })} />
         </table>
     )
 }
