018701763cf48fcda57371d39c8bdea443254753
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-22 10:01:50 +0200

    [update] table style check

--- src/components/songTable.tsx
+++ src/components/songTable.tsx
@@ -1,45 +1,47 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
-import { albumDataArr, albumInt } from '../utils/album.data';
-import DeleteData from './modals/deleteData'
 import DeleteSongData from './modals/deleteSongData';
 
-const SongTable = ({data}:{data:any}) => {
+const SongTable = ({ data }: { data: any }) => {
     const [modal, setModal] = useState({
-        id:'',
-        showModal:false
+        id: '',
+        showModal: false
     });
     const navigate = useNavigate();
     const handleUpdate = (id: number) => {
         navigate(`/songs/${id}/update`);
     }
     return (
-        <table className=' w-full'>
-            <tr className='text-gray-500 outline-2 outline-dashed h-12 border-gray-500 text-[14px]'>
-                <th>#</th>
-                <th>Name of Song</th>
-                <th>Artist </th>
-                <th>Time</th>
-                <th>Genre</th>
-                <th>Actions</th>
-            </tr>
-            {data.map((song: any, i: number) => (
-                <tr key={song.song_id} className="border-b-2 hover:bg-slate-900 text-center hover:cursor-pointer border-gray-500 text-[12px] font-semibold">
-                    <td className='text-center'>
-                        # {i}
-                    </td>
-                    <td className='flex gap-8 py-5 justify-center place-items-center text-center'>
-                        <span>{song.title}</span>
-                    </td>
-                    <td className='text-center'>{song.artist}</td>
-                    <td className='text-center'>{song.length}</td>
-                    <td className='text-center'>{song.genre}</td>
-                    <td className='text-center justify-center'>
-                        <button onClick={() => handleUpdate(i)} className='bg-main hover:bg-white hover:text-main duration-500 border-2 border-white opacity-95 mx-4 text-black py-1 px-6 text-[12px] rounded-md'>Update</button>
-                        <button onClick={() => setModal({ ...modal, id: song.song_id, showModal: true })} className='bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
-                    </td>
+        <table className='w-full'>
+            <thead className='border-solid border-b-2 h-12 border-gray-700 '>
+                <tr className='text-gray-500 text-[14px]'>
+                    <th>#</th>
+                    <th>Name of Song</th>
+                    <th>Artist </th>
+                    <th>Time</th>
+                    <th>Genre</th>
+                    <th>Actions</th>
                 </tr>
-            ))}
+            </thead>
+            <tbody>
+                {data.map((song: any, i: number) => (
+                    <tr key={i} className="border-b-2 text-slate-200 font-light hover:bg-main text-center hover:cursor-pointer group hover:border-white duration-500 border-gray-500 text-[12px]">
+                        <td className='text-center'>
+                            # {i + 1}
+                        </td>
+                        <td className='flex font-bold gap-8 py-5 justify-start mx-20 place-items-center text-center'>
+                            <span>{song.title}</span>
+                        </td>
+                        <td className='text-left'>{song?.artist}</td>
+                        <td className='text-left'>{song?.length}</td>
+                        <td className='text-left'>{song?.genre}</td>
+                        <td className='text-center justify-center'>
+                            <button onClick={() => { navigate(`/albums/${song.song_id}/update`) }} className='bg-main hover:bg-white hover:text-main duration-500 border-2 border-white opacity-95 mx-4 text-black py-1 px-6 text-[12px] rounded-md'>Update</button>
+                            <button onClick={() => setModal({ ...modal, id: song.song_id, showModal: true })} className='group-hover:bg-white group-hover:text-main bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
             <DeleteSongData id={modal.id} showModal={modal.showModal} onClose={() => setModal({ ...modal, showModal: false })} />
         </table>
     )
