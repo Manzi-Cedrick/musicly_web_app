d9d9badcf0f19eba5cc4e4a8a643ab1cfafeb32b
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-22 10:00:47 +0200

    [update] album style updates

--- src/components/albumTable.tsx
+++ src/components/albumTable.tsx
@@ -12,30 +12,30 @@ const AlbumTable = ({data}:{data:any}) => {
     });
     return (
         <table className='w-full'>
-            <thead>
-                <tr className='text-gray-500 outline-2 outline-dashed h-12 border-gray-500 text-[14px]'>
+            <thead className='border-solid border-b-2 h-12 border-gray-700 '>
+                <tr className='text-gray-500 text-[14px]'>
                     <th>#</th>
-                    <th>Name of Song</th>
-                    <th>Description </th>
+                    <th>Album Name</th>
+                    <th>Short Description </th>
                     <th>Release date</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
                 {data.map((album: albumInt, i: number) => (
-                        <tr key={i} className="border-b-2 hover:bg-slate-900 text-center hover:cursor-pointer border-gray-500 text-[12px] font-semibold">
+                        <tr key={i} className="border-b-2 text-slate-200 font-light hover:bg-main text-center hover:cursor-pointer group hover:border-white duration-500 border-gray-500 text-[12px]">
                             <td className='text-center'>
                                 # {i + 1}
                             </td>
-                            <td className='flex gap-8 py-5 justify-center place-items-center text-center'>
-                                <img className='w-8 h-8 object-cover rounded-full' src={`${album.cover_image}`} alt="" />
+                            <td className='flex font-bold gap-8 py-5 justify-start mx-20 place-items-center text-center'>
+                                <img className='w-8 h-8 object-cover rounded-sm' src={`${album.cover_image}`} alt="" />
                                 <span>{album.title}</span>
                             </td>
-                            <td className='text-center'>{album.description}</td>
-                            <td className='text-center'>{album?.release_date}</td>
+                            <td className='text-left'>{album?.description}</td>
+                            <td className='text-left'>{album?.release_date}</td>
                             <td className='text-center justify-center'>
                                 <button onClick={() => { navigate(`/albums/${album.album_id}/update`) }} className='bg-main hover:bg-white hover:text-main duration-500 border-2 border-white opacity-95 mx-4 text-black py-1 px-6 text-[12px] rounded-md'>Update</button>
-                                <button onClick={() => setModal({ ...modal, id: album.album_id, showModal: true })} className='bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
+                                <button onClick={() => setModal({ ...modal, id: album.album_id, showModal: true })} className='group-hover:bg-white group-hover:text-main bg-transparent hover:bg-slate-700 hover:text-main duration-500 border-2 border-main text-main py-1 px-6 text-[12px] rounded-md'>Delete</button>
                             </td>
                         </tr>
                     ))
